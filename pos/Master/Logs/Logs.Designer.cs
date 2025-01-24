26588dd70a27ddcaa533b060a5889070c06f94a4
ahsankhan87 <ahsankhan50@gmail.com>
2025-01-24 23:18:21 +0500

    remove log from gitignore

---
+++ pos/Master/Logs/Logs.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace pos.Master.Logs
+{
+    partial class Logs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label21 = new System.Windows.Forms.Label();
+            this.GridLogs = new System.Windows.Forms.DataGridView();
+            this.BranchId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UserId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Action = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Details = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Timestamp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PcName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AdditionalInfo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.toDate = new System.Windows.Forms.DateTimePicker();
+            this.fromDate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnSearch = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridLogs)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.panel1.Controls.Add(this.BtnSearch);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.fromDate);
+            this.panel1.Controls.Add(this.toDate);
+            this.panel1.Controls.Add(this.label21);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(961, 66);
+            this.panel1.TabIndex = 0;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.ForeColor = System.Drawing.Color.White;
+            this.label21.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label21.Location = new System.Drawing.Point(13, 22);
+            this.label21.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(203, 25);
+            this.label21.TabIndex = 18;
+            this.label21.Text = "All Application Logs";
+            // 
+            // GridLogs
+            // 
+            this.GridLogs.AllowUserToAddRows = false;
+            this.GridLogs.AllowUserToDeleteRows = false;
+            this.GridLogs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridLogs.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.BranchId,
+            this.UserId,
+            this.Action,
+            this.Details,
+            this.Timestamp,
+            this.PcName,
+            this.AdditionalInfo});
+            this.GridLogs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GridLogs.Location = new System.Drawing.Point(0, 0);
+            this.GridLogs.Name = "GridLogs";
+            this.GridLogs.ReadOnly = true;
+            this.GridLogs.RowHeadersWidth = 51;
+            this.GridLogs.RowTemplate.Height = 24;
+            this.GridLogs.Size = new System.Drawing.Size(961, 709);
+            this.GridLogs.TabIndex = 0;
+            // 
+            // BranchId
+            // 
+            this.BranchId.DataPropertyName = "BranchId";
+            this.BranchId.HeaderText = "Branch Id";
+            this.BranchId.MinimumWidth = 6;
+            this.BranchId.Name = "BranchId";
+            this.BranchId.ReadOnly = true;
+            this.BranchId.Width = 125;
+            // 
+            // UserId
+            // 
+            this.UserId.DataPropertyName = "UserId";
+            this.UserId.HeaderText = "User Id";
+            this.UserId.MinimumWidth = 6;
+            this.UserId.Name = "UserId";
+            this.UserId.ReadOnly = true;
+            this.UserId.Width = 125;
+            // 
+            // Action
+            // 
+            this.Action.DataPropertyName = "Action";
+            this.Action.HeaderText = "Action";
+            this.Action.MinimumWidth = 6;
+            this.Action.Name = "Action";
+            this.Action.ReadOnly = true;
+            this.Action.Width = 125;
+            // 
+            // Details
+            // 
+            this.Details.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Details.DataPropertyName = "Details";
+            this.Details.HeaderText = "Details";
+            this.Details.MinimumWidth = 6;
+            this.Details.Name = "Details";
+            this.Details.ReadOnly = true;
+            // 
+            // Timestamp
+            // 
+            this.Timestamp.DataPropertyName = "Timestamp";
+            this.Timestamp.HeaderText = "TimeStamp";
+            this.Timestamp.MinimumWidth = 6;
+            this.Timestamp.Name = "Timestamp";
+            this.Timestamp.ReadOnly = true;
+            this.Timestamp.Width = 125;
+            // 
+            // PcName
+            // 
+            this.PcName.DataPropertyName = "PcName";
+            this.PcName.HeaderText = "PC Name";
+            this.PcName.MinimumWidth = 6;
+            this.PcName.Name = "PcName";
+            this.PcName.ReadOnly = true;
+            this.PcName.Width = 125;
+            // 
+            // AdditionalInfo
+            // 
+            this.AdditionalInfo.DataPropertyName = "AdditionalInfo";
+            this.AdditionalInfo.HeaderText = "Additional Info";
+            this.AdditionalInfo.MinimumWidth = 6;
+            this.AdditionalInfo.Name = "AdditionalInfo";
+            this.AdditionalInfo.ReadOnly = true;
+            this.AdditionalInfo.Width = 125;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.GridLogs);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 66);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(961, 709);
+            this.panel2.TabIndex = 1;
+            // 
+            // toDate
+            // 
+            this.toDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.toDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.toDate.Location = new System.Drawing.Point(765, 22);
+            this.toDate.Name = "toDate";
+            this.toDate.Size = new System.Drawing.Size(101, 22);
+            this.toDate.TabIndex = 1;
+            // 
+            // fromDate
+            // 
+            this.fromDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fromDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.fromDate.Location = new System.Drawing.Point(623, 22);
+            this.fromDate.Name = "fromDate";
+            this.fromDate.Size = new System.Drawing.Size(101, 22);
+            this.fromDate.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(575, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(732, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 17);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "To";
+            // 
+            // BtnSearch
+            // 
+            this.BtnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnSearch.Location = new System.Drawing.Point(874, 22);
+            this.BtnSearch.Name = "BtnSearch";
+            this.BtnSearch.Size = new System.Drawing.Size(75, 25);
+            this.BtnSearch.TabIndex = 2;
+            this.BtnSearch.Text = "Search";
+            this.BtnSearch.UseVisualStyleBackColor = true;
+            this.BtnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // Logs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 775);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Name = "Logs";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logs";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Logs_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Logs_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridLogs)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView GridLogs;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BranchId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Action;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Details;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Timestamp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PcName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AdditionalInfo;
+        private System.Windows.Forms.Button BtnSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker fromDate;
+        private System.Windows.Forms.DateTimePicker toDate;
+    }
+}
---
+++ pos/Master/Logs/Logs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pos.Master.Logs
+{
+    public partial class Logs : Form
+    {
+        public Logs()
+        {
+            InitializeComponent();
+        }
+
+        private void Logs_Load(object sender, EventArgs e)
+        {
+            GridLogs.AutoGenerateColumns = false;
+           
+            GridLogs.DataSource =  POS.DLL.Log.GetAll();
+        }
+
+        private void Logs_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GridLogs.AutoGenerateColumns = false;
+
+                GridLogs.DataSource = POS.DLL.Log.SearchRecordByDate(fromDate.Value.Date, toDate.Value.Date);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Log Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+            
+        }
+    }
+}
